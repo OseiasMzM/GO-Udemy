ba82d32c23ab2306fbeb4a447da5d2b5e6d8c75b
Oséias Mozdzen Martins <61160846+OseiasMzM@users.noreply.github.com>
2023-07-26 15:56:18 -0300

    Aula finalizada

---
+++ Pacotes/auxiliar/auxiliar.go
@@ -0,0 +1,9 @@
+package auxiliar
+
+import "fmt"
+
+// Registra uma mensagem no console
+func Escrever() {
+	fmt.Println("Escrevendo do pacote Auxiliar")
+	escrever2()
+}
---
+++ hello.go
@@ -0,0 +1,9 @@
+package main //É necessário para identificar o pacote principal e definir o ponto de entrada do programa.
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Olá Mundo!👋")
+}
