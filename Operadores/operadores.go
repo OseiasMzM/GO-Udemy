2dd5dc1ae1e8735fa335f1f8f42da78482af764b
Oséias Mozdzen Martins <61160846+OseiasMzM@users.noreply.github.com>
2023-07-31 00:32:51 -0300

    Aula - Operadores

---
+++ Operadores/operadores.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Aritmeticos
+	soma := 1 + 2
+	subtracao := 1 - 2
+	divisao := 10 / 4
+	multiplicacao := 10 * 5
+	restoDaDivisao := 10 % 2
+
+	fmt.Println(soma, subtracao, divisao, multiplicacao, restoDaDivisao)
+}
